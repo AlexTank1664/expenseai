6933626c9a49f396c64b26e6f7958eabef284c79
Alex Mironov <mironovs@mail.ru>
2025-08-18 21:19:29 +0300

    Initial Commit

---
+++ expenseai/expenseaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseaiApp.swift
+//  expenseai
+//
+//  Created by MacbookPro on 18.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct expenseaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expenseaiTests/expenseaiTests.swift
@@ -0,0 +1,16 @@
+//
+//  expenseaiTests.swift
+//  expenseaiTests
+//
+//  Created by MacbookPro on 18.08.2025.
+//
+
+import Testing
+
+struct expenseaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
