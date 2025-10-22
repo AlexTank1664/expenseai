a786850e89d76b9da0d7ee0c0dbbdf5249b75dbc
Alex Mironov <mironovs@mail.ru>
2025-10-22 07:57:22 +0300

    feat: deleted particapants fixed

--- expenseai/ParticipantsListView.swift
+++ expenseai/ParticipantsListView.swift
@@ -23,10 +23,15 @@ struct ParticipantsListView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         entity: Participant.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \Participant.name, ascending: true)]
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \Participant.isSoftDeleted, ascending: true),
+            NSSortDescriptor(keyPath: \Participant.name, ascending: true)
+        ]
     ) var participants: FetchedResults<Participant>
     
     @State private var activeSheet: ActiveSheet?
+    @State private var showingDeleteConfirmation = false
+    @State private var offsetsToDelete: IndexSet?
     @EnvironmentObject var authService: AuthService
     @EnvironmentObject private var localizationManager: LocalizationManager
 
@@ -34,7 +39,9 @@ struct ParticipantsListView: View {
         List {
             ForEach(participants, id: \.id) { participant in
                 Button(action: {
-                    activeSheet = .participantEditor(participant)
+                    if !participant.isSoftDeleted {
+                        activeSheet = .participantEditor(participant)
+                    }
                 }) {
                     VStack(alignment: .leading, spacing: 4) {
                         HStack {
@@ -42,7 +49,7 @@ struct ParticipantsListView: View {
                                 .font(.headline)
                             
                             if participant.isSoftDeleted {
-                                Text("(to be deleted)")
+                                Text("(" + localizationManager.localize(key: "marked as deleted")  + ")")
                                     .font(.caption)
                                     .foregroundColor(.red)
                             }
@@ -68,9 +75,20 @@ struct ParticipantsListView: View {
                     }
                     .padding(.vertical, 4)
                 }
+                .disabled(participant.isSoftDeleted)
                 .foregroundColor(.primary)
+                .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                    if participant.isSoftDeleted {
+                        Button {
+                            restoreParticipant(participant)
+                        } label: {
+                            Label("Restore", systemImage: "arrow.uturn.backward")
+                        }
+                        .tint(.green)
+                    }
+                }
             }
-            .onDelete(perform: deleteParticipants)
+            .onDelete(perform: confirmDelete)
         }
         .navigationTitle(localizationManager.localize(key: "Participants"))
         .toolbar {
@@ -99,6 +117,35 @@ struct ParticipantsListView: View {
                 ContactPickerView()
             #endif
             }
+        }
+        .alert(isPresented: $showingDeleteConfirmation) {
+            Alert(
+                title: Text("Confirm Deletion"),
+                message: Text("Are you sure you want to delete this participant? A participant can only be deleted if they are not involved in any group as a payer or a recipient."),
+                primaryButton: .destructive(Text("Delete")) {
+                    if let offsets = offsetsToDelete {
+                        deleteParticipants(offsets: offsets)
+                    }
+                },
+                secondaryButton: .cancel()
+            )
+        }
+    }
+
+    private func confirmDelete(offsets: IndexSet) {
+        self.offsetsToDelete = offsets
+        self.showingDeleteConfirmation = true
+    }
+
+    private func restoreParticipant(_ participant: Participant) {
+        guard let userID = authService.currentUser?.id else { return }
+        withAnimation {
+            participant.objectWillChange.send()
+            participant.isSoftDeleted = false
+            participant.updatedAt = Date()
+            participant.updatedBy = Int64(userID)
+            participant.needsSync = true
+            try? viewContext.save()
         }
     }
 
